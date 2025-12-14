e3963204938e36eac0cb17ead0ff92d74b13a6e8
Даник <pistriuga.danylo@lll.kpi.ua>
2025-12-15 00:33:38 +0200

    Implement buttons for image manipulation: add, enlarge, reduce, remove

--- Lab4/script.js
+++ Lab4/script.js
@@ -2,6 +2,8 @@
 
 const table = document.getElementById("hobbies");
 const h4 = document.querySelector(".books-title");
+const img = document.querySelector("img")
+const buttons = document.querySelector(".button-container")
 
 const getRandomHexColor = function () {
 	return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6)
@@ -10,9 +12,35 @@ const getRandomHexColor = function () {
 table.addEventListener("click", (e) => {
 	e.currentTarget.style.color = getRandomHexColor();
 	e.currentTarget.style.backgroundColor = getRandomHexColor();
-})
+});
 
 h4.addEventListener("click", (e) => {
 	e.currentTarget.style.color = getRandomHexColor();
 	e.currentTarget.style.backgroundColor = getRandomHexColor();
-})
+});
+
+const operation = new Map([
+	['add', (el) => {
+		el.style.display = "block";
+	}],
+	['enlarge', (el) => {
+		if (Number(el.height) < 3) el.height = "3"
+		if (Number(el.width) < 6) el.width = "6"
+		el.height = String(Number(el.height) * 1.25);
+		el.width = String(Number(el.width) * 1.25);
+	}],
+	['reduce', (el) => {
+		el.height = String(Number(el.height) / 1.25);
+		el.width = String(Number(el.width) / 1.25);
+	}],
+	['remove', (el) => {
+		el.style.display = "none";
+	}]
+])
+
+buttons.addEventListener("click", (e) => {
+	const op = e.target.className;
+	console.log(op)
+	const action = operation.get(op);
+	action(img);
+});
